e145bfeb902355553bed90fe4408c9b8a83c9850
bogland <guruwang2@gmail.com>
2019-10-08 02:29:09 +0900

    캐릭터 애니메이션 프리팹 지정 및 /Util/Scene/SceneEffect 모듈 분할 예시 제작

---
+++ Tiga/Assets/Util/Scene/SceneEffect/SceneEffector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneEffector
+{
+    public CanvasGroup panel;
+    // Use this for initialization
+    public float durationTime = 5f;
+
+    public SceneEffector(CanvasGroup panel)
+    {
+        this.panel = panel;
+    }
+
+    public void setTime(float durationTime)
+    {
+        this.durationTime = durationTime;
+    }
+    public IEnumerator FadeOut()
+    {
+        float time = 0;
+        while (panel.alpha > 0f)
+        {
+            time += Time.deltaTime / durationTime;
+            panel.alpha = Mathf.Lerp(1, 0, time);
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeIn()
+    {
+        float time = 0;
+        while (panel.alpha < 1f)
+        {
+            time += Time.deltaTime / durationTime;
+            panel.alpha = Mathf.Lerp(0, 1, time);
+            yield return null;
+        }
+    }
+
+}
---
+++ Tiga/Assets/Util/Scene/SceneEffect/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SceneTest
+{
+    public class SceneManager : MonoBehaviour
+    {
+        public CanvasGroup TransitionPanel;
+        // Use this for initialization
+        void Start()
+        {
+            SceneEffector sceneEffector = new SceneEffector(TransitionPanel);
+            StartCoroutine(sceneEffector.FadeOut());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
